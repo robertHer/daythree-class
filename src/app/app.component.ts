2319f814fc404ae9a9feb7a6fe2b2281c52002dd
unknown <totolero769@gmail.com>
2018-05-31 16:13:31 -0700

    Day 3 typescript practice 1 and 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 
-class Point{
-  x:number;
-  y:number;
+// class Point{
+//   x:number;
+//   y:number;
 
-  constructor(x:number , y:number){
-  this.x = y;
-  this.y = x;
+//   constructor(x:number , y:number){
+//   this.x = x;
+//   this.x = y;
   
-}
-add(point:Point){
-  return new Point (this.x + point.x , this.y + point.y);
-}
+// }
+// add(point:Point){
+//   return new Point (this.x + point.x , this.y + point.y);
+// }
 
-}
+// }
 
 
-class Point3D extends Point{
-  z:number;
+// class Point3D extends Point{
+//   z:number;
   
-  constructor(x:number , y:number, z:number){
-super(x,y);
-this.z = z;
+//   constructor(x:number , y:number, z:number){
+// super(x,y);
+// this.z = z;
+// }
+//   add(new3DPoint : Point3D){
+//     return new Point3D (this.x + new3DPoint.x , this.y + new3DPoint.y ,this.z + new3DPoint.z );
+//   }
+// }
+
+interface Person {
+  firstName:string;
+  lastName:string;
+
+  sayHi: ()=> string;
+   
 }
-  add(new3DPoint : Point3D){
-    return new Point3D (this.x + new3DPoint.x , this.y + new3DPoint.y ,this.z + new3DPoint.z );
-  }
-}
+
+
 
 @Component({
   selector: 'app-root',
@@ -40,30 +50,50 @@ export class AppComponent implements OnInit {
 
   ngOnInit(){
   
-    this.classPointTest();
-    this.extendPointTest();
+    // this.classPointTest();
+    // this.extendPointTest();
+    this.practice2Test();
 
   }
 
 
 
 
-classPointTest(){
-  var p1 =  new Point (10,3);
-  var p2 = new Point (5,5);
- var p3 = p1.add(p2);
+// classPointTest(){
+//   var p1 =  new Point (10,3);
+//   var p2 = new Point (5,5);
+//  var p3 = p1.add(p2);
 
- console.log(p3);
+//  console.log(p3);
+// }
+
+// extendPointTest(){
+//   const p3d1 = new Point3D (4,5,3);
+//   const p3d2 = new Point3D (5,7,2);
+//   const p3d3 =  p3d1.add(p3d2);
+
+//   console.log(p3d3);
+// }
+
+practice2Test(){
+
+  const customer:Person = { 
+    firstName:"bob",
+    lastName:"smith",
+    sayHi: ():string =>{return "Hi there"} 
+ } 
+ console.log(customer.firstName, customer.lastName , customer.sayHi()) 
+ 
+ 
+ var employee:Person = { 
+    firstName:"jamie",
+    lastName:"smith", 
+    sayHi: ():string =>{return "Hello!!!"} 
+ } 
+   
+ console.log(employee.firstName, employee.lastName, employee.sayHi()) 
+
 }
 
-extendPointTest(){
-  const p3d1 = new Point3D (4,5,3);
-  const p3d2 = new Point3D (5,7,2);
-  const p3d3 =  p3d1.add(p3d2);
 
-  console.log(p3d3);
 }
-
-
-
-
